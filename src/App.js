c10a229b92a5fec0809098c5acddc31d79b496b0
[guswls1788] <[guswls1788@naver.com]>
2022-02-16 19:00:57 +0900

    five commit

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component{
   render(){
     
     return(
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
 
       <div id="app">
  
@@ -102,12 +102,12 @@ class App extends Component{
             <li><a href="#"></a></li>
           </ul>
         </div>
-        <Routes basename={process.env.PUBLIC_URL}>
+        <Routes>
           <Route exact path="/" exact={true} element={<Home/>} />
           <Route path="/search" exact={true} element={<Search searchText={this.state.searchText} getdetail={this.getdetail} get={this.get}/>} />
         </Routes>
         </div>
-      </ HashRouter>
+      </ BrowserRouter>
     )
   }
 }
